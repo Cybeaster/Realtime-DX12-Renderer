29a1941b5fcc5014ded92213a33a6280b96493cb
Cybeaster <cybeastermail@gmail.com>
2023-12-09 17:26:33 +0400

    Added dx12 helpers

---
+++ Types/Types.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm> // For std::min and std::max.
+#include <cassert>
+#include <chrono>
+#include <cstdint>
+#include <queue>
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+using std::array;
+using std::make_shared;
+using std::pair;
+using std::queue;
+using std::shared_ptr;
+using std::string;
+using std::vector;
+using std::weak_ptr;
+using std::wstring;
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#define WIN32_LEAN_AND_MEAN
+
+#define _DEBUG
+
+#include "DXHelper.h"
+
+#include <Application.h>
+#include <Shlwapi.h>
+#include <Test/SimpleCubeTest/SimpleCubeTest.h>
+#include <Windows.h>
+#include <dxgidebug.h>
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+	int returnCode = 0;
+
+	const HMODULE hModule = GetModuleHandleW(NULL);
+
+	if (WCHAR path[MAX_PATH]; GetModuleFileNameW(hModule, path, MAX_PATH) > 0)
+	{
+		PathRemoveFileSpecW(path);
+		SetCurrentDirectoryW(path);
+	}
+
+	const auto application = OApplication::Get();
+	application->InitApplication(hInstance);
+
+	returnCode = application->Run<OSimpleCubeTest>();
+	application->Destory();
+
+	atexit(&ReportLiveObjects);
+	return returnCode;
+}
